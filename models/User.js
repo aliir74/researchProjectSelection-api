5816e8594898b7785dafdae6ee6fff3061dd1e2e
aliir74 <aliirani74@gmail.com>
2017-09-25 22:37:34 +0330

    change User model

--- models/User.js
+++ models/User.js
@@ -3,7 +3,10 @@ const Schema = mongoose.Schema
 
 var UserSchema = new Schema({
     username: String,
-    password: String
+    password: String,
+    grade: Number,
+    name: String,
+    projects: [{priority: Number, name: String}]
 })
 
 module.exports = mongoose.model('User', UserSchema)
