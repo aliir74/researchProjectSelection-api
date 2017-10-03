f9726fb86089a11fb70d8b0aa2d9cf65db5e0128
aliir74 <aliirani74@gmail.com>
2017-10-03 08:41:51 +0330

    add addProjects route

--- server.js
+++ server.js
@@ -124,6 +124,42 @@ server.register(Basic, (err) => {
         }
     })
 
+    server.route({
+        method: 'POST',
+        path: '/addprojects/{grade}',
+        config: {
+            payload: {
+                output: 'stream',
+                parse: true,
+                allow: 'multipart/form-data'
+            },
+            handler: function (request, reply) {
+                var data = request.payload;
+                if (data.file) {
+                    var name = data.file.hapi.filename;
+                    var path = __dirname + "/uploads/" + name;
+                    var file = fs.createWriteStream(path);
+
+                    file.on('error', function (err) {
+                        console.error(err)
+                    });
+
+                    data.file.pipe(file);
+
+                    data.file.on('end', function (err) {
+                        var ret = {
+                            filename: data.file.hapi.filename,
+                            headers: data.file.hapi.headers
+                        }
+                        var buf = fs.readFileSync(path)
+                        console.log(buf)
+                        reply(JSON.stringify(ret));
+                    })
+                }
+            }
+        }
+    })
+
 
 
     server.start( (err) => {
